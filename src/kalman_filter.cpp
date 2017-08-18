a0a7df281d4c60d381b55f4d46a082971616fc1f
AlexQian97 <wenzhangq@gmail.com>
2017-08-17 21:35:35 -0600

    first attempt

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -22,6 +22,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +32,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +52,39 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float x = x_[0];
+  float y = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+
+  float rho = sqrt(x * x + y * y);
+  float phi = atan2(y, x);
+
+  float ro_dot = (x * vx + y * vy) / rho;
+  VectorXd z_predict = VectorXd(3);
+  z_predict << rho, phi, ro_dot;
+
+  // Error calculation for prediction vs actual 
+  // z_predict is the converted cordinates from the prediction step (see above)
+  VectorXd y_error = z - z_predict;
+
+  // Normalize phi
+  while (y_error[1] > M_PI) {
+     y_error[1] -= 2 * M_PI;
+  }
+  while (y_error[1] < -M_PI) {
+    y_error[1] += 2 * M_PI;
+  }
+
+  // Lesson 5.7
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  // Set the new state/uncertainty
+  x_ = x_ + (K * y_error);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
